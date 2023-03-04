c47160ab1145ff1b413438c5a4140c8314830f6f
Niya Shiyas <niyashiyas03@gmail.com>
2023-03-04 12:38:59 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import NavBar from "./Components/NavBar/NavBar.js";
 import "./App.css";
-import {originals, action} from "./urls.js";
+import {action, originals, comedy} from "./urls";
 import Banner from "./Components/Banner/Banner.js";
 import RowPost from "./Components/RowPost/RowPost.js";
 
@@ -12,6 +12,7 @@ function App() {
       <Banner />
       <RowPost url={originals} title="Netflix Originals" />
       <RowPost url={action} title="Action" isSmall />
+      <RowPost url={comedy} title="Comedy" isSmall />
     </div>
   );
 }
--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -5,26 +5,23 @@ import axios from "../../axios";
 function RowPost(props) {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
-    axios
-      .get(props.url)
-      .then((response) => {
-        console.log(response.data.results);
-        setMovies(response.data.results);
-      })
-      .catch((err) => {});
+    axios.get(props.url).then((response) => {
+      console.log(response.data.results);
+      setMovies(response.data.results);
+    });
   }, []);
 
   return (
     <div className="row">
       <h2>{props.title}</h2>
       <div className="posters">
-        {movies.map((obj) => {
+        {movies.map((obj) => (
           <img
             className={props.isSmall ? "smallPoster" : "poster"}
             src={`${imageUrl + obj.backdrop_path}`}
             alt="poster"
-          />;
-        })}
+          />
+        ))}
       </div>
     </div>
   );
