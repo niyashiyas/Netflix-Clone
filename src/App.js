9f1398e56a19bd3436f07927b3fc87441258ca98
Niya Shiyas <niyashiyas03@gmail.com>
2023-03-04 20:32:33 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 import NavBar from "./Components/NavBar/NavBar.js";
 import "./App.css";
-import {action, originals, comedy} from "./urls";
+import {
+  action,
+  originals,
+  comedy,
+  horror,
+  romance,
+  documentaries,
+  trending,
+} from "./urls";
 import Banner from "./Components/Banner/Banner.js";
 import RowPost from "./Components/RowPost/RowPost.js";
 
@@ -10,9 +18,13 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
-      <RowPost url={originals} title="Netflix Originals" />
+      <RowPost url={originals} title="NETFLIX ORIGINALS" />
+      <RowPost url={trending} title="Trending Now" isSmall />
       <RowPost url={action} title="Action" isSmall />
       <RowPost url={comedy} title="Comedy" isSmall />
+      <RowPost url={horror} title="Horror" isSmall />
+      <RowPost url={romance} title="Romance" isSmall />
+      <RowPost url={documentaries} title="Documentaries" isSmall />
     </div>
   );
 }
